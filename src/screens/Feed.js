6679775ad2ee053ab6b3a2fd4148c71f03ab5580
joannapo <jp@MacBook-Pro.local>
2022-06-10 17:51:02 +0200

    my v2

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,82 +1,84 @@
 import * as React from 'react';
-import { Image, StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { Image, Text, View, FlatList, SafeAreaView, StyleSheet } from 'react-native';
+
 
 const divider = () => {
+
   return (
     <View
-      style={{
-        height: 10,
-        width: "100%",
-        backgroundColor: 'white',
-      }}
+      style={styles.divider}
     />
   );
 }
 
-const styleSheet = StyleSheet.create
-(
-  {
-  MainContainer: {
-    flex: 1,
-  },
-  listItem: {
-    paddingLeft: 12,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  itemText: {
-    fontSize: 24,
-    color: 'black',
-    fontFamily: 'Cochin',
-  },
-  activityIndicator: {
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-  }
-  }
-);
+function Feed() {
 
-function Feed() 
-{
   const [data, setData] = React.useState([])
-  React.useEffect(() => 
-  {
+  
+  React.useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos")
       .then((res) => res.json())
-      .then((response) => 
-      {
+      .then((response) => {
         setData(response)
       })
-      .catch((error) => 
-      {
+      .catch((error) => {
         console.log(error)
       })
   }, [])
 
-  const renderItem = ({ item }) => 
-  {
-    console.log(item)
+  const renderItem = ({ item }) => {
     return (
-      <View style={{ flex: 1, flexDirection: "column", margin: 1 }}>
-        <Image style={{ height: 300, width: '100%'}} source={{ uri: item.url }} />
-    <Text style={{ fontSize: 18, color: 'black' }}>{item.title}</Text> 
+      <View style={styles.container}>
+        <Image style={styles.Image} source={{ uri: item.url }} />
+        <Text>{item.title}</Text>
       </View>
     )
   }
 
   return (
-    <SafeAreaView style={styleSheet.MainContainer}>
-          <FlatList 
-            data={data} renderItem={renderItem}
-            ItemSeparatorComponent={divider}
-            />
+    <SafeAreaView style={styles.MainContainer}>
+      <FlatList
+        data={data} renderItem={renderItem}
+        ItemSeparatorComponent={divider}
+      />
     </SafeAreaView>
   )
 }
-  
-  export default Feed
+const styles = StyleSheet.create
+  (
+    {
+      divider :{
+        height: 10,
+        width: "100%",
+        backgroundColor: 'white',
+      },
+      Image: {
+        height: 300,
+        width: '100%',
+      },
+      MainContainer: {
+        flex: 1,
+      },
+      listItem: {
+        paddingLeft: 12,
+        paddingTop: 10,
+        paddingBottom: 10,
+      },
+      itemText: {
+        fontSize: 24,
+        color: 'black',
+        fontFamily: 'Cochin',
+      },
+      activityIndicator: {
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      }
+    }
+  );
+
+export default Feed
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,102 +1,115 @@
 import React from 'react';
-import { StyleSheet, View, Text, FlatList, Image, TouchableOpacity, Modal, SafeAreaView} from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity, Modal, SafeAreaView, StyleSheet, useWindowDimensions} from 'react-native';
 
 import SearchBox from '../../src/screenComponents/SearchBox';
 
-function ShowGrid() 
-{
-  //Hook do pokazania / schowania modal view
-const [getModal, setModal] = React.useState(false)
-  //Hook do get / set adresu zdjecia
-const [getImageUrl, setImageUrl] = React.useState('')
-  //Hook do get / set tytulu zdjecia
-const [getImageTitle, setImageTitle] = React.useState('')
-  //Funkcja, ktora robi get / set tytulu, adresu oraz modal view
-const toggleModal = (tytul, url) => {
-    setImageUrl(url);
-    setImageTitle(tytul);
-    setModal(!getModal);
-}
-  //Hook do get / set pobranych danych (zdjec)
-const [data, setData] = React.useState([])
 
-  //Pobieranie zdjec
-React.useEffect(() => 
-{
-    fetch("https://jsonplaceholder.typicode.com/photos")
-    .then((res) => res.json())
-    .then((response) => 
-    {
-        setData(response)
-    })
-    .catch((error) => 
-    {
-        console.log(error)
-    })
-}, [])
 
-return (
-    //Widok grid zdjec oraz modal z widokiem konkretnego zdjecia
-    <View style={styles.MainContainer}>     
-    <SearchBox /> 
-    <FlatList
-        data={data}
-        renderItem={({item}) => 
-            <View style={styles.GridViewBlockStyle}>
-            <TouchableOpacity onPress={() => {toggleModal(item.title, item.url)}}>
-                <Image style={{ height: 100, width: 190 }} source={{ uri: item.url}} ></Image>
-            </TouchableOpacity> 
-            </View>
-        }
-        
-    numColumns={2}       
-    />
+
+function ShowGrid() {
+    //Hook do pokazania / schowania modal view
+    const [getModal, setModal] = React.useState(false)
+    //Hook do get / set adresu zdjecia
+    const [getImageUrl, setImageUrl] = React.useState('')
+    //Hook do get / set tytulu zdjecia
+    const [getImageTitle, setImageTitle] = React.useState('')
+    //Funkcja, ktora robi get / set tytulu, adresu oraz modal view
+    const toggleModal = (tytul, url) => {
+        setImageUrl(url);
+        setImageTitle(tytul);
+        setModal(!getModal);
+    }
+    //Hook do get / set pobranych danych (zdjec)
+    const [data, setData] = React.useState([])
+    const window = useWindowDimensions()
     
-    <Modal visible={getModal}>
-        <SafeAreaView>
-        <TouchableOpacity onPress={toggleModal}>
-            <Image style={{ height: 300, width: 420 }} source={{uri: getImageUrl}}/>
-            <Text style={{fontSize: 20, marginLeft: 10, marginRight: 10, top: 400}}>{getImageTitle}</Text>
-        </TouchableOpacity>
-        </SafeAreaView>
-    </Modal>
-    </View>
-)
-}
+    
+    //Pobieranie zdjec
+    React.useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/photos")
+            .then((res) => res.json())
+            .then((response) => {
+                setData(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
 
+    return (
+        
+        //Widok grid zdjec oraz modal z widokiem konkretnego zdjecia
+        <SafeAreaView style={styles.MainContainer}>
+            <SearchBox />
+            <FlatList style={{width: window.width}}
+                data={data}
+                renderItem={({ item }) =>
+                    <View style={styles.GridViewBlockStyle}>
+                        <TouchableOpacity onPress={() => { toggleModal(item.title, item.url) }}>
+                            <Image style={styles.ImageFlatList} source={{ uri: item.url }} ></Image>
+                        </TouchableOpacity>
+                    </View>
+                }
+
+                numColumns={2}
+            />
+
+            <Modal visible={getModal}>
+                <SafeAreaView >
+                    <TouchableOpacity onPress={toggleModal}>
+                        <Image style={{height: window.height*0.5, width: window.width}} source={{ uri: getImageUrl }} />
+                        <Text style={styles.TextModal}>{getImageTitle}</Text>
+                    </TouchableOpacity>
+                </SafeAreaView>
+            </Modal>
+        </SafeAreaView>
+    )
+}
 const styles = StyleSheet.create({
 
-MainContainer :{
-justifyContent: 'center',
-flex:1,
-margin: 10,
-},
+    TextModal: {
+        fontSize: 20,
+        marginLeft: 10, 
+        marginRight: 10, 
+        top: 10
+    },
 
-ActivityIndicator_Style:{
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center'
-},
+    ImageFlatList: {
+        height: 100, 
+        width: 190,
+    },
 
-GridViewBlockStyle: {
-    justifyContent: 'center',
-    flex:1,
-    alignItems: 'center',
-    height: 100,
-    margin: 5,
-    backgroundColor: 'white',
-},
+    MainContainer: {
+        justifyContent: 'center',
+        flex: 1,
+        margin: 0,
+    },
 
-GridViewInsideTextItemStyle: {
-    color: '#fff',
-    padding: 10,
-    fontSize: 18,
-    justifyContent: 'center',
-},
+    ActivityIndicator_Style: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    GridViewBlockStyle: {
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        height: 100,
+        margin: 1,
+        backgroundColor: 'white',
+    },
+
+    GridViewInsideTextItemStyle: {
+        color: '#fff',
+        padding: 1,
+        fontSize: 18,
+        justifyContent: 'center',
+    },
 });
 
 export default ShowGrid
